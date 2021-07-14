92095a60c2afe2ad5e81ebc919cc689bd9d3b34d
paladinescamila <camilapaladines27@gmail.com>
2021-07-14 18:50:28 -0500

    Completed calendar settings

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // ELEMENTS
-const monthOptionsContainer = document.getElementById("select-month");
+const monthOptionsContainer = document.getElementById("select-input");
 const monthContainer = document.getElementById("month");
 const monthYearContainer = document.getElementById("month-year");
 const leftButtonElement = document.getElementById("left-button");
 const rightButtonElement = document.getElementById("right-button");
+const goDateElement = document.getElementById("go-date");
+const dayValue = document.getElementById("day-value");
+const yearValue = document.getElementById("year-value");
 
 // CONSTANTS
 const monthsNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -15,17 +18,27 @@ let date = new Date();
 let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
-let isLeap = year % 400 === 0 ? true : year % 100 === 0 ? false : year % 4 === 0;
-if (isLeap) {
-	monthDaysNumbers[1] = 29;
-}
 
 // Add months options
 let monthOptions = "";
 for (let i = 0; i < 12; i++) {
-	monthOptions += `<option>${monthsNames[i]}</option>`;
+	monthOptions += `<option value=${i + 1}>${monthsNames[i]}</option>`;
 }
 monthOptionsContainer.innerHTML = monthOptions;
+
+// Know month start day
+const getStartDay = (month, year) => {
+	const date = new Date(year, month - 1, 1);
+	return date.getDay();
+};
+
+// Set leap year
+const setLeap = () => {
+	let isLeap = year % 400 === 0 ? true : year % 100 === 0 ? false : year % 4 === 0;
+	monthDaysNumbers[1] = isLeap ? 29 : 28;
+};
+
+setLeap();
 
 // Set calendar settings
 const setCalendar = () => {
@@ -36,8 +49,8 @@ const setCalendar = () => {
 	}
 
 	// Add month days
-	let monthDays = "";
-	for (let i = 0; i < monthDaysNumbers[month - 1]; i++) {
+	let monthDays = `<div class="month-day" style="grid-column-start:${getStartDay(month, year)};">1</div>`;
+	for (let i = 1; i < monthDaysNumbers[month - 1]; i++) {
 		monthDays += `<div class="month-day">${i + 1}</div>`;
 	}
 
@@ -55,6 +68,7 @@ leftButtonElement.addEventListener("click", (e) => {
 	} else {
 		month -= 1;
 	}
+	setLeap();
 	setCalendar();
 });
 
@@ -66,5 +80,14 @@ rightButtonElement.addEventListener("click", (e) => {
 	} else {
 		month += 1;
 	}
+	setLeap();
+	setCalendar();
+});
+
+goDateElement.addEventListener("click", (e) => {
+	day = dayValue.value;
+	month = monthOptionsContainer.value;
+	year = yearValue.value;
+	setLeap();
 	setCalendar();
 });
